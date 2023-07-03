6a4f4ce9178004708cfd68dcdf3e4e8984bb1140
Alberto Rocha <betogrun@gmail.com>
2023-07-03 15:01:46 -0300

    Remove comments

--- app/lib/plain_case.rb
+++ app/lib/plain_case.rb
@@ -24,36 +24,3 @@ class PlainCase
   def Failure(result, type = :error) = Result::Failure.new(result, type)
 end
 
-
-
-# class MyOperation < PlainCase
-#   def initialize(input:)
-#     @input = input
-#   end
-
-#   def call
-#     if @input > 0
-#       Success(@input)
-#     else
-#       Failure("Input must be positive", type: :input_error)
-#     end
-#   end
-# end
-
-# result = MyOperation.call(input: -1)
-
-# # Pattern matching with Array
-# case result
-# in [:success, value, *]
-#   puts "Operation was successful, value is #{value}"
-# in [:failure, message, *]
-#   puts "Operation failed with message: #{message}"
-# end
-
-# # Pattern matching with Hash
-# case result
-# in {status: :success, result: value}
-#   puts "Operation was successful, value is #{value}"
-# in {status: :failure, result: message, type: _}
-#   puts "Operation failed with message: #{message}"
-# end
